798f0baf3d29b99781b11a06c368d133fdf0c061
Julien Bonnanfant <julienbonnanfant@Juliens-MacBook-Pro.local>
2019-06-04 10:23:57 +0200

    Crection des Scripts 1 et 2 et ajout de echo

--- scripts/import_github.sh
+++ scripts/import_github.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+
+echo "Telechargement du json"
 wget https://data.gharchive.org/2018-05-07-15.json.gz
-gunzip  2018-05-07-15.json.gz
-nano  2018-05-07-15.json
+
+echo "Extraction du Json"
+gunzip  2018-05-07-15.json.gz -y
+
+echo "Deplacement du json"
+cp 2018-05-07-15.json ../json/2018-05-07-15.json
+rm â2018-05-07-15.json
 
 
+
+nano  ../json/2018-05-07-15.json
--- scripts/setup_postgresql.sh
+++ scripts/setup_postgresql.sh
@@ -6,9 +6,19 @@ echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" > /etc/apt
 
 #import de la clés de syndchronisation
 
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-     sudo apt-get update
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -    
+sudo apt-get update
 
-#Installation de postgresQL
+#Installation de postgresQL 11 
 
-sudo apt-get install postgresql-11 | psql --version
+sudo apt-get install postgresql-11 -y 
+
+echo "Your Psql version is :"
+psql --version
+
+echo "Starting postgresql server.."
+
+sudo service postgresql start
+sudo service postgresql status
+
+echo "Setup postgresQL finish"
