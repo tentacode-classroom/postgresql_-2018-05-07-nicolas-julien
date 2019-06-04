6920767aa27217c3ac3dafa0bde601a8f9f1d80b
Neerfix <nicolas.notararigo@ynov.com>
2019-06-04 15:14:19 +0200

    sql script and change

--- scripts/sql/github_events.sql
+++ scripts/sql/github_events.sql
@@ -5,6 +5,31 @@ CREATE DATABASE github_events;
 
 DROP TABLE IF EXISTS events;
 CREATE TABLE events (
-  name varchar(255)
+  event_id serial,
+  type varchar(255),
+  actor_id bigint(20),
+  repo_id bigint(20),
+  PRIMARY KEY (`event_id`),
+  KEY `actor` (`actor_id`),
+  KEY `repo_id` (`repo_id`)
 );
-SELECT * FROM events;
+
+DROP TABLE IF EXISTS watch_events;
+CREATE TABLE watch_events (
+  watch_event_id serial,
+) INHERITS (events);
+
+DROP TABLE IF EXISTS push_events;
+CREATE TABLE push_events (
+  push_event_id serial,
+) INHERITS (events);
+
+DROP TABLE IF EXISTS actor
+CREATE TABLE actor(
+  actor_id serial,
+)
+
+DROP TABLE IF EXISTS repo
+CREATE TABLE repo(
+  repo_id serial,
+)
