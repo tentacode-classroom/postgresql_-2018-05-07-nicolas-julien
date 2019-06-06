d846dcd1ee787a43a34fdfae190a75a2e22170a2
Neerfix <nicolas.notararigo@ynov.com>
2019-06-06 11:52:05 +0200

    github event

--- scripts/sql/github_events.sql
+++ scripts/sql/github_events.sql
@@ -3,14 +3,25 @@ CREATE DATABASE github_events;
 
 \c github_events;
 
+CREATE TABLE actor(
+	actor_id serial,
+	login varchar(45) NOT NULL,
+	display_login varchar(45) NOT NULL,
+	url varchar(255) NOT NULL,
+	avatar_url varchar(255) NOT NULL
+);
+
+CREATE TABLE repo(
+	repo_id serial,
+	name varchar(255) NOT NULL
+	url varchar(255) NOT NULL
+);
+
 CREATE TABLE events (
-	event_id serial,
+	id serial PRIMARY KEY,
 	type varchar(255),
 	actor_id bigint(20),
-	repo_id bigint(20),
-	PRIMARY KEY (`event_id`),
-	KEY `actor` (`actor_id`),
-	KEY `repo_id` (`repo_id`)
+	repo_id bigint(20)
 );
 
 CREATE TABLE watch_events (
@@ -20,20 +31,3 @@ CREATE TABLE watch_events (
 CREATE TABLE push_events (
 	push_event_id serial,
 ) INHERITS (events);
-
-CREATE TABLE actor(
-	actor_id serial,
-	login varchar(45) NOT NULL,
-	display_login varchar(45) NOT NULL,
-	url varchar(255) NOT NULL,
-	avatar_url varchar(255) NOT NULL
-)
-
-CREATE TABLE repo(
-	repo_id serial,
-	name varchar(255) NOT NULL
-	url varchar(255) NOT NULL
-)
-
-INSERT INTO events ('', 'commit', '1', '5')
-INSERT INTO actor ('', 'Michel', 'Michel', 'http:\/\/www.michel.com')
