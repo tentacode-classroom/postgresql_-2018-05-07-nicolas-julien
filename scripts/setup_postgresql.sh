fb5a6408e39e49c52c93f763298fa9f6a954863e
Neerfix <nicolas.notararigo@ynov.com>
2019-06-06 10:34:00 +0200

    fix error

--- scripts/setup_postgresql.sh
+++ scripts/setup_postgresql.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 #Ajout du repsository
-
-echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" | sudo tee --append /etc/apt/sources.list.d/pgdg.list
 
 #import de la clés de syndchronisation
 
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -    
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
 
-#Installation de postgresQL 11 
+#Installation de postgresQL 11
 
-sudo apt-get install postgresql-11 -y 
+sudo apt-get install postgresql-11 -y
 
 echo "Your Psql version is :"
 psql --version
